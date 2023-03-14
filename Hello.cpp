0448fed76298de01c41727bd42d391504b6d38c5
LimDoeon <doeonlim8656@gmail.com>
2023-03-14 17:49:18 +0900

    v2

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 // int main(void)
 // {
 //     std::string myStr;
@@ -36,24 +35,53 @@
 //     return 0;
 // }
 
+// using namespace std;
+
+// namespace first {
+//     int x = 5;
+//     int y = 10;
+// }
+
+// namespace second {
+//     double x = 3.1416;
+//     double y = 2.7183;
+// }
+
+// int main () {
+//     {
+//     using namespace first;
+//     cout << x << '\n';
+//     }
+//     {
+//     using namespace second;
+//     cout << y << '\n';
+//     }
+
+//     return 0;
+// }
+
+// default values in functions
+// using namespace std;
+// int divide (int a, int b=2){
+//     int r;
+//     r = a/b;
+//     return (r);
+// }
+
+// int main (){
+//     cout << divide (12) <<'\n';
+//     cout << divide (20, 4) << '\n';
+//     return 0;
+// }
+
+// range-based for loop
+#include <iostream>
+#include <string>
 using namespace std;
-
-namespace first {
-    int x = 5;
-    int y = 10;
-}
-
-namespace second {
-    double x = 3.1416;
-    double y = 2.7183;
-}
-
-int main () {
-    using namespace first;
-    cout << x << '\n';
-    cout << y << '\n';
-    cout << first::y << '\n';
-    cout << second::x << '\n';
-
-    return 0;
+int main() {
+    string myStr {"Hello!"};
+    for (auto c : myStr) {
+        cout << "["<< c << "]";
+    }
+    cout << '\n';
 }
